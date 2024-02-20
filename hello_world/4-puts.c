76c2c28eb85839cc6bc225b99f1ed6964abcdf43
Quent-Aliaslerouqin <8791@holbertonstudents.com>
2024-02-20 09:51:57 +0100

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - code to put a message
- * Return: Always 0
- */
+* main - code to put a message
+* Return: Always 0
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

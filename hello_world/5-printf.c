77b8ef362769d4812420332d65c10842f9e1f8ba
Quent-Aliaslerouqin <8791@holbertonstudents.com>
2024-02-20 10:06:29 +0100

    5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - code to put a message
+* Return: Always 0
+*/
+int main(void)
+{
+    printf(with proper grammar, but the outcome is a piece of art,);
+    return(0);
+}

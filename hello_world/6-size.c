caa8c31cf9911a024183f2e12209bad35a86f1d2
Quent-Aliaslerouqin <8791@holbertonstudents.com>
2024-02-20 16:08:52 +0100

    test

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,9 +6,9 @@
 int main(void)
 {
 	printf("Size of char: %d byte(s)\n", sizeof(char));
-    printf("Size of int: %d byte(s)\n", sizeof(int));
-    printf("Size of float: %d byte(s)\n", sizeof(float));
-    printf("Size of long: %d byte(s)\n", sizeof(long));
-    printf("Size of long long: %d byte(s)\n", sizeof(long long));
-    return(0);
+	printf("Size of int: %d byte(s)\n", sizeof(int));
+	printf("Size of float: %d byte(s)\n", sizeof(float));
+	printf("Size of long: %d byte(s)\n", sizeof(long));
+	printf("Size of long long: %d byte(s)\n", sizeof(long long));
+	return (0);
 }

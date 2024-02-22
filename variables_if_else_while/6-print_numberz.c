ac6cd65ea40464d87af73b263c89a5ba28d7e930
Quent-Aliaslerouqin <8791@holbertonstudents.com>
2024-02-22 13:54:20 +0100

    6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - code to put a message
+* Return: Always 0
+*/
+int main(void)
+{
+	char num;
+	{
+		for (num = 0; num < 10; num++)
+
+		putchar(num);
+	}
+	putchar("\n");
+	return (0);
+}

f4e4136b09567216ff91dcc9cecbb79fb1664d52
Quent-Aliaslerouqin <8791@holbertonstudents.com>
2024-02-23 15:51:44 +0100

    2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,14 +9,13 @@ void print_alphabet_x10(void)
 {
 	int a;
 	char alphabet;
+
 	for (a = 0; a < 10; a++)
-	
 	{
 		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		{
 			_putchar(alphabet);
 		}
-
 		_putchar('\n');
 	}
 }

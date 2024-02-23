5486f4ac872c044a02717c07698fe297308550ba
Quent-Aliaslerouqin <8791@holbertonstudents.com>
2024-02-23 12:33:35 +0100

    1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <unistd.h>
+/**
+*  main - Print putchar, followed by a new line.
+*
+*Return: Always 0.
+*/
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+	{
+		_putchar(alphabet);
+
+	}
+
+	_putchar('\n');
+}

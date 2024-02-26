41c8acd16751387f401d72f00d83f94b1ef22734
Quent-Aliaslerouqin <8791@holbertonstudents.com>
2024-02-26 15:25:13 +0100

    6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,17 @@
 #include "main.h"
 
 /**
-* more_numbers - a
+* print_line(int n)- a
 * Return: a
 */
 
+void print_line(int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+	{
+	_putchar('_');
+	}
+
+	_putchar('\n');
+}

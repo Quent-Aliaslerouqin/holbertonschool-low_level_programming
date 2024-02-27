9d7d484ff3a500c1fc6ac1fde1485b0a96698bb8
Quent-Aliaslerouqin <8791@holbertonstudents.com>
2024-02-27 09:45:51 +0100

    10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* print_triangle- a
+* @size: a
+* Return: a
+*/
+void print_triangle(int size)
+{
+	int i;
+	int t;
+	int c;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+	{
+		for (t = 0; t <= (size - i - 1); i++)
+		{
+			_putchar(' ');
+		}
+	for (c = 0; c <= i - 1; c++)
+		{
+		_putchar('#');
+		}
+	_putchar('\n');
+	}
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}

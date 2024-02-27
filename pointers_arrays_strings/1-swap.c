d28d6fd07b8503d79717dd9617a7dc918a8cf799
Quent-Aliaslerouqin <8791@holbertonstudents.com>
2024-02-27 15:03:14 +0100

    1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * reset_to_98 - check the code
- *
- * @n: tg
+ * swap_int - check the code
+ * @b: encore tg
+ * @a: tg
  * Return: Always 0.
  */
 
 void swap_int(int *a, int *b)
 {
 	int c;
-	
+
 	c = *a;
 	*a = *b;
 	*b = c;

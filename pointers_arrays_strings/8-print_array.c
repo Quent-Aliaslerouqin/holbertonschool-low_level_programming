ce135b5652443f60d29379343a2badfa7b5de957
Quent-Aliaslerouqin <8791@holbertonstudents.com>
2024-03-03 19:16:45 +0100

    10

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - r
+ * @a: r
+ * @n: r
+ *
+ */
+void print_array(int *a, int n)
+{
+        int i;
+
+        for (i = 0; i < n; i++)
+        {
+                printf("%d", a[i]);
+                if (i != n - 1)
+                        printf(", ");
+        }
+        printf("\n");
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -6,5 +6,10 @@ void rev_string(char *s);
 int _putchar(char a);
 void print_square(int size);
 int _strlen(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+int _atoi(char *s);
+char *_strcpy(char *dest, char *src);
 
 

561a0974875445da60df21fdc1a9bb873df914ad
Quent-Aliaslerouqin <8791@holbertonstudents.com>
2024-03-05 11:47:24 +0100

    0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,38 @@
+
+#include "main.h"
+/**
+ * _memset - cc
+ * @s: cc
+ * @b: cc
+ * @n: cc
+ *
+ * Return: a
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+    unsigned int len = 0;
+    while (len < n)
+    {
+        s[len] = b;
+        len++;
+    }
+    return (s);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,15 +1,3 @@
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-int _putchar(char a);
-void print_square(int size);
-int _strlen(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-int _atoi(char *s);
-char *_strcpy(char *dest, char *src);
+char *_memset(char *s, char b, unsigned int n);
 
 

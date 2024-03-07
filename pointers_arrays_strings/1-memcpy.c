68f93f59fd6884147e5900e2cb53e0c55abe01c5
Quent-Aliaslerouqin <8791@holbertonstudents.com>
2024-03-07 12:34:40 +0100

    0

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - cc
+ * @s: cc
+ * @b: cc
+ * @n: cc
+ *
+ * Return: a
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	unsigned int len = 0;
+
+	while (len < n)
+	{
+
+
+	}
+

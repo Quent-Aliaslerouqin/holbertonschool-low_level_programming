17a28699ffafc263cb8b191f107ce1c9a9c37cca
Quent-Aliaslerouqin <8791@holbertonstudents.com>
2024-06-19 10:29:12 +0200

    nom

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - ouf
+* Return: always 0
+* puts - lol
+*/
+
+int main (void)
+{
+    str = "Programming is like building a multilingual puzzle";
+    puts(str);
+    return(0);
+}

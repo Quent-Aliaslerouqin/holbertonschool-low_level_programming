792fcb38d4f2a10c54434c2ab59f0175372a9f7e
Quent-Aliaslerouqin <8791@holbertonstudents.com>
2024-06-25 10:58:10 +0200

    0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - entry point 
+* Return : 0
+**/
+int main()
+
+{
+	char c;
+
+	c = 'C';
+	printf("Return value when uppercase character %c is passed to isupper(): %d", c, isupper(c));
+
+	c = '+';
+	printf("\nReturn value when another character %c is passed to is isupper(): %d", c, isupper(c));
+
+	return 0;
+}

184b9611ded7d7947da8c3ef0b4d2141eb26284b
Quent-Aliaslerouqin <8791@holbertonstudents.com>
2024-06-25 14:46:43 +0200

    create 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* isupper - hrt
+* main - entry point 
+* Return : 0	if (c >= 65 && c <= 90)
+
+**/
+int _isdigit(int c)
+
+{
+
+	if (c >= 48 && c <= 57)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
